3445ec136bb69d711fac5489bc2b7cad12295fba
Rosyec Parrado <chechomens@gmail.com>
2026-01-10 16:47:33 -0500

    delete console.log

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,6 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor(private readonly configService: ConfigService) {
         const connectionString = configService.get<string>('DATABASE_URL');
-        console.log('DATABASE_URL:', connectionString);
         const adapter = new PrismaPg({ connectionString });
 
         super({ adapter });
